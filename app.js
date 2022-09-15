f3e3c7f8f803c64cce26f627cc71eb985bacbd4a
JM <julien.mangeat@mail-formateur.net>
2022-09-15 11:33:17 +0200

    ajout loggeur sur création / connexion

--- app.js
+++ app.js
@@ -15,15 +15,14 @@ app.listen(HTTP_PORT, ()=>{
 let db = new sqlite3.Database('./model/bdd.db', (err)=>{
     if(err){
         console.error("J'ai pas pu ouvrir la BDD " + err.message)
+        logger.log({level : 'error', message : 'BDD indisponible' })
     }else{
         db.run('CREATE TABLE employees(employee_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, last_name NVARCHAR(20) NOT NULL, first_name NVARCHAR(20) NOT NULL, title NVARCHAR(20), cp INTEGER(5))', ()=>{
-            let insert = "INSERT INTO employees (last_name, first_name, title, cp) VALUES (?,?,?,?)"
-            db.run(insert, ["Chamoix", "Dore", "DG", 75000])
-            db.run(insert, ["Idris", "Maloi", "SE", 75000])
-            db.run(insert, ["Jhon", "Do", "AG", 75000])
+            logger.log({level : 'info', message : 'Création de la BDD'})
         })
     }
     console.log("La BDD est disponible")
+    logger.log({level : 'info', message : 'La BDD est disponible'})
 })
 
 //Demande d'information sur id d'employée
